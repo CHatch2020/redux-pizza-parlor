65a910301f75a339cf4c42fca89d3c417368ee08
Caleb Hatch <hatchcaleb@gmail.com>
2021-12-02 13:45:04 -0600

    started pizza list wireframe

---
+++ src/components/PizzaList/PizzaList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+
+function PizzaList() {
+    return(
+        <div>
+            <p>Put List Here</p>
+        </div>
+    )
+};
+
+export default PizzaList;
