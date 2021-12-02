fd9e861772a6e749a1da5d67207b9cf930caa80c
Caleb Hatch <hatchcaleb@gmail.com>
2021-12-02 14:44:58 -0600

    fixed divs

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -2,29 +2,27 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 
-import './PizzaList.css';
+import "./PizzaList.css";
 import PizzaItem from "../PizzaItem/PizzaItem";
-import CustomerForm from '../CustomerForm/CustomerForm';
+import CustomerForm from "../CustomerForm/CustomerForm";
 
 function PizzaList() {
   const getPizzaList = useSelector((store) => store.pizzaListReducer);
 
   return (
     <div>
-             <h2>Step 1: Select Your Pizza</h2>
-            {getPizzaList.map((pizza, i) => {
-                return <PizzaItem key={i} pizza={pizza}/>
-            })}
-    </div>
-        <Router>
-          <Route exact path="/form">
-            <CustomerForm />
-          </Route>
-          <Link to="/form">
-            <button>Next</button>
-          </Link>
-        </Router>
-      </div>
+      <h2>Step 1: Select Your Pizza</h2>
+      {getPizzaList.map((pizza, i) => {
+        return <PizzaItem key={i} pizza={pizza} />;
+      })}
+      <Router>
+        <Route exact path="/form">
+          <CustomerForm />
+        </Route>
+        <Link to="/form">
+          <button>Next</button>
+        </Link>
+      </Router>
     </div>
   );
 }
